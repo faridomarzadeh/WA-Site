5afa0b1bc466419d038a83e8c5cba6de649d7bdb
Farid Omarzadeh <farid.omarzadeh29@gmail.com>
2024-07-24 00:22:29 -0400

    added js for resume

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,16 @@
 let navToggleIcon = document.getElementsByClassName('nav__toggle-icon')[0];
 let menu = document.getElementsByClassName('menu')[0];
 let cover = document.querySelector('.cover');
-
+let resumeListItems = document.querySelectorAll('.resume-list__item');
 navToggleIcon.addEventListener('click', () =>{
     navToggleIcon.classList.toggle('nav__toggle-icon--open');
     menu.classList.toggle('menu--open');
     cover.classList.toggle('cover--show');
+})
+
+resumeListItems.forEach(resumeListItem =>{
+    resumeListItem.addEventListener('click', function(){
+        document.querySelector('.resume-list__item--active').classList.remove('resume-list__item--active');
+        this.classList.add('resume-list__item--active');
+    })
 })
